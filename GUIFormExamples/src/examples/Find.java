2c590b5920544121b7bd5aa8da33447f504f226c
Cris <cris@cris.com>
2017-02-16 17:54:21 +0100

    sout

--- GUIFormExamples/src/examples/Find.java
+++ GUIFormExamples/src/examples/Find.java
@@ -155,6 +155,7 @@ public class Find extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+                System.out.println("Cristian");
                 new Find().setVisible(true);
             }
         });
